4be8fe1ee0d5a7cab12462be3b83ee66eac23d86
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-04-21 08:06:03 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 /**
  * main - Entry point
  *
- * Description: false or true validation.
+ * Description: Determines if a number is positive, negative or zero.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,10 +16,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > o)
-		printf("%i is positive\n", n);
+	{
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-		printf("%i is zero\n", n);
+	{
+		printf("%d is zero\n", n);
+	}
 	else
-		printf("%i is negative\n", n);
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
