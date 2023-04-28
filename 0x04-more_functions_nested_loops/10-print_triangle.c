66299ed9cd20431ef58567c25a55106b88677fe4
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-04-27 17:14:27 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (hght = 1, hght <= size, hght++)
+		for (hght = 1; hght <= size; hght++)
 		{
-			for (base = 1, base <= size, base++)
+			for (base = 1; base <= size; base++)
 			{
 				if ((hght + base) <= size)
 					_putchar(' ');
