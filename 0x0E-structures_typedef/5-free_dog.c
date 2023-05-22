41563c9dceae78e6aa800a3f9a5b46256ae8565a
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-05-22 13:47:24 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees yo dawgs
+ * @d: yo dawg
+ *
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
