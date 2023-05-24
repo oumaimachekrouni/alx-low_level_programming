99f76d83eaddd5d127010f3509748f0cc75ab1c8
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-05-24 10:32:03 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -35,6 +35,15 @@ int main(int argc, char **argv)
 		return (100);
 	}
 
-	printf("%d\n", op_func(a, b));
+	if (op_func != NULL) /* Additional check */
+	{
+		printf("%d\n", op_func(a, b));
+	}
+	else
+	{
+		printf("Error\n");
+		return (99);
+	}
+
 	return (0);
 }
