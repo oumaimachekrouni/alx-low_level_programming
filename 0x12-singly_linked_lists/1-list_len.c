ca63dba70273c7906a54b23f0dfd56026b4b7994
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-06-01 16:59:37 -0700

    1-list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - determines length of linked list
+ * @h: pointer to first node
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
