4b243c2ab3ae586bb2d08f5bb28256f1da4293f2
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-06-06 08:49:47 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - prints a linked lists
+ * @h: pointer to first node
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -14,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
