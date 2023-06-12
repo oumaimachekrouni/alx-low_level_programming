cba18a425f98736b6cbed95a216e4f032d41c5a6
oumaimachekrouni <oumaima.chekrouni2017@gmail.com>
2023-06-12 10:47:57 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,37 +17,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd = open(filename, O_RDONLY);
-	char *buffer = (char *)malloc(letters * sizeof(char));
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
 
-	if (filename == NULL || letters == 0)
+	if (!filename || !letters)
 		return (0);
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-
-	ssize_t bytesRead = read(fd, buffer, letters);
-
-	if (bytesRead == -1)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-
-	if (bytesWritten == -1 || bytesWritten != bytesRead)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILEND, &buf[0], bytes);
 	close(fd);
-	free(buffer);
 
-	return (bytesRead);
+	return (bytes);
 }
